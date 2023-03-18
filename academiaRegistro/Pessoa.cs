8de1eaa137785b9772d513b58b814966aebc53f7
João Vitor Delaia <115042912+M4ntov4@users.noreply.github.com>
2023-03-17 21:40:10 -0300

    Add files via upload

---
+++ academiaRegistro/Pessoa.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace academiaRegistro
+{
+    class Pessoa
+    {
+        string nome;
+        int idade;
+        double peso;
+        double altura;
+        double imc;
+
+        public string Nome { get => nome; set => nome = value; }
+        public int Idade { get => idade; set => idade = value; }
+        public double Peso { get => peso; set => peso = value; }
+        public double Altura { get => altura; set => altura = value; }
+        public double Imc { get => imc; set => imc = value; }
+
+        public Pessoa(string nome, int age, double weight, double height)
+        {
+            this.Nome = nome;
+            this.Idade = age;
+            this.Peso = weight;
+            this.Altura = height;
+            
+        }
+
+        // Para Alterar os Atributos
+
+        public void AlterarNome(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        public void Alteraridade(int idade)
+        {
+            this.Idade = idade;
+        }
+
+        public void AlterarPeso(double peso)
+        {
+            this.Peso = peso;
+        }
+
+        public void AlterarAltura(double altura)
+        {
+            this.Altura = altura;
+        }
+
+           // Para consultar os Atributos
+
+        public string LerNome()
+        {
+            return this.Nome;
+        }
+
+        public int LerIdade()
+        {
+            return this.Idade;
+        }
+
+        public double LerPeso()
+        {
+            return this.Peso;
+        }
+
+        public  double LerAltura()
+        {
+            return this.Altura;
+        }
+
+        public double LerImc()
+        {
+            return this.Imc;
+        }
+
+        // para calcular imc
+
+        public void CalcularImc()
+        {
+
+            this.Imc = this.peso / (this.altura * this.altura);
+
+        }
+
+
+
+    }
+}
---
+++ academiaRegistro/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace academiaRegistro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome;
+            int idade;
+            double peso;
+            double altura;
+
+            Console.Write("Cadastre seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Cadastre sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.Write("insira peso: ");
+            peso = double.Parse(Console.ReadLine());
+
+            Console.Write("insira a altura (ex: 1,80cm): ");
+            altura = double.Parse(Console.ReadLine());
+
+            
+
+
+            Pessoa cliente = new Pessoa(nome,idade,peso,altura); // recebe os parametros (caracteristicas do objeto)
+
+            cliente.CalcularImc(); // faz o calculo de imc
+
+            
+            
+            Console.WriteLine("Seu nome é: " + cliente.LerNome());
+            Console.WriteLine("Sua idade é: " + cliente.LerIdade());
+            Console.WriteLine("Seu peso é: " + cliente.LerPeso());
+            Console.WriteLine("Sua Altura é: " + cliente.LerAltura());
+            Console.WriteLine("Seu IMC é: " + cliente.LerImc());
+
+            Console.ReadKey();
+        }
+    }
+}
